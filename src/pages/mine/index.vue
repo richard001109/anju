5dc5c3c89492881c7d236ade32d1fc68dc2d2046
tang <2973654845@qq.com>
2022-07-12 11:52:41 +0800

    7/12 am

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,10 +1,31 @@
 <template>
-    <view>我的</view>
+    <view>我的
+        <view @click="gotoCollect">我的收藏</view>
+        <view @click="gotoBeholder">申请入驻</view>
+    </view>
+
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            gotoCollect(){
+                uni.navigateTo({
+                    url:"./mine_collect/index"
+                })
+            },
+            gotoBeholder(){
+                uni.navigateTo({
+                    url:"./mine_beholder/index"
+                })
+            }
+        }
     }
 </script>
 
---
+++ src/pages/mine/mine_collect/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <view>
+        <view class="title">我的收藏</view>
+        <view class="content" v-for="item in houses" :key="item.id">
+            <image mode="widthFix" :src="item.imgurl"></image>
+
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return{
+                houses:[{
+                    imgurl:"https://tse1-mm.cn.bing.net/th/id/OIP-C.n0_p3rYRuofABd3XudbZnAHaEo?w=278&h=180&c=7&r=0&o=5&dpr=1.12&pid=1.7",
+                    title:"大平层，户型方正，碧桂园时代倾城",
+                    shi:3,
+                    ting:2,
+                    area:184.94,
+                    orientation:"南"
+
+                },
+                    {
+                        imgurl:"https://tse2-mm.cn.bing.net/th/id/OIP-C.1CyIihS--FMRpXQsydJWJQHaEo?w=278&h=180&c=7&r=0&o=5&dpr=1.12&pid=1.7",
+                        title:"大平层，户型方正，碧桂园时代倾城",
+                        shi:3,
+                        ting:2,
+                        area:184.94,
+                        orientation:"南"
+
+                    },
+                    {
+                        imgurl:"https://tse1-mm.cn.bing.net/th/id/OIP-C.irADmH6QxTb8VQKXtZE5PwHaEo?w=300&h=187&c=7&r=0&o=5&dpr=1.12&pid=1.7",
+                        title:"大平层，户型方正，碧桂园时代倾城",
+                        shi:3,
+                        ting:2,
+                        area:184.94,
+                        orientation:"南"
+
+                    },
+                    {
+                        imgurl:"https://tse4-mm.cn.bing.net/th/id/OIP-C.kRAf0rLDyb8XhT2JKYUwzAHaEK?w=287&h=180&c=7&r=0&o=5&dpr=1.12&pid=1.7",
+                        title:"大平层，户型方正，碧桂园时代倾城",
+                        shi:3,
+                        ting:2,
+                        area:184.94,
+                        orientation:"南"
+
+                    },
+                    {
+                        imgurl:"https://tse3-mm.cn.bing.net/th/id/OIP-C.ICOKxTBjrWt_-YskkYRNXgHaEo?w=246&h=180&c=7&r=0&o=5&dpr=1.12&pid=1.7",
+                        title:"大平层，户型方正，碧桂园时代倾城",
+                        shi:3,
+                        ting:2,
+                        area:184.94,
+                        orientation:"南"
+
+                    }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
