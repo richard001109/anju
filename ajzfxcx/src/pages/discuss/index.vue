4c26b2ba5b6c1de1c2f2990935113068b5763cbc
tang <2973654845@qq.com>
2022-07-12 10:31:04 +0800

    项目创建

---
+++ ajzfxcx/src/pages/discuss/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <view>敬请期待</view>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ajzfxcx/src/pages/rent/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <view>租房</view>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
